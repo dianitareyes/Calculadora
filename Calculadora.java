9650870a524cf70701c5b828010a098ba0d0dd75
Minorusal <minorusal@hotmail.com>
2024-12-09 10:46:05 -0600

    Se declaro while y se agregaron las opciones del menu

--- Calculadora.java
+++ Calculadora.java
@@ -5,5 +5,16 @@ public class Calculadora {
         Scanner scanner = new Scanner(System.in);
         int opcion;
         boolean salir = false;
+
+        while (!salida){
+            System.out.println("=== Menú de Calculadora ===");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicación");
+            System.out.println("4. División (Cociente)");
+            System.out.println("5. División (Residuo)");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opción: ");
+        }
     }
 }
