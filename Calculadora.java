2e0ffc7aff5d6e4d6ee662050dbba2e8d12489a1
Minorusal <minorusal@hotmail.com>
2024-12-09 10:42:24 -0600

    Se declaran variable opcion y salir

--- Calculadora.java
+++ Calculadora.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int opcion;
+        boolean salir = false;
     }
 }
