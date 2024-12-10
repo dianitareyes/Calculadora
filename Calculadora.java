f54a5d48a52789e93124e184ca1c6af7954f468e
Minorusal <minorusal@hotmail.com>
2024-12-09 18:03:50 -0600

    Realizar la operación seleccionada utilizando un switch y manejar errores en la división para evitar cálculos inválidos

--- Calculadora.java
+++ Calculadora.java
@@ -19,11 +19,38 @@ public class Calculadora {
              opcion = scanner.nextInt();
 
             if(opcion >= 1 && opcion <= 5){
-                
+
                 System.out.print("Ingrese el primer número: ");
                 double num1 = scanner.nextDouble();
                 System.out.print("Ingrese el segundo número: ");
                 double num2 = scanner.nextDouble();
+
+
+                 switch (opcion) {
+                    case 1: // Suma
+                        System.out.println("Resultado de la suma: " + (num1 + num2));
+                        break;
+                    case 2: // Resta
+                        System.out.println("Resultado de la resta: " + (num1 - num2));
+                        break;
+                    case 3: // Multiplicación
+                        System.out.println("Resultado de la multiplicación: " + (num1 * num2));
+                        break;
+                    case 4: // División 
+                        if (num2 != 0) {
+                            System.out.println("Resultado de la división: " + (num1 / num2));
+                        } else {
+                            System.out.println("Error: No se puede dividir entre cero.");
+                        }
+                        break;
+                    case 5: // División 
+                        if (num2 != 0) {
+                            System.out.println("Residuo de la división: " + (num1 % num2));
+                        } else {
+                            System.out.println("Error: No se puede dividir entre cero.");
+                        }
+                        break;
+                }
 
             }
 
